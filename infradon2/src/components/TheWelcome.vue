a5db3878e5eced3ccb9da6211a18058d2598ed44
Camilo <paula@pena-parra.ch>
2025-10-02 15:39:32 +0200

    création fichiers vue.js et premiers essais

---
+++ infradon2/src/components/TheWelcome.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+const counter = ref(0);
+
+const increment = () => {
+  counter.value++;
+}
+
+const reset = () => {
+  counter.value = 0;
+}
+
+</script>
+
+<template>
+  <h1>Hello World</h1>
+  <p>{{  counter  }}</p>
+  <button @click="increment">increment</button>
+  <button @click="reset">reset</button>
+</template>
