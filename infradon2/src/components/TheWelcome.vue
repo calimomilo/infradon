3937456392e750fc610ac0a237b750c839fba072
Camilo <paula@pena-parra.ch>
2025-10-30 15:08:46 +0100

    added CRUD functionalities

--- infradon2/src/components/TheWelcome.vue
+++ infradon2/src/components/TheWelcome.vue
@@ -1,40 +1,114 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import PouchDB from 'pouchdb';
+import { ref, onMounted } from 'vue'
+import PouchDB from 'pouchdb'
 
-const storage = ref();
+declare interface Post {
+  _id: string
+  _rev: string
+  title: string
+  content: string
+  attributes: {
+    creation_date: any
+  }
+}
+
+const storage = ref()
+const postsData = ref<Post[]>([])
 
 onMounted(() => {
-  console.log('=> Composant initialisé');
-  initDatabase();
-});
+  console.log('=> Composant initialisé')
+  initDatabase()
+  fetchData()
+})
 
 const initDatabase = () => {
-  console.log('=> Connexion à la base de données');
-  const db = new PouchDB('http://calimo:c@lim=m0m!lo@localhost:5984/infradon2')
+  console.log('=> Connexion à la base de données')
+  const db = new PouchDB('http://calimo:admin@localhost:5984/infradon2')
   if (db) {
-    console.log("Connected to collection : " + db?.name)
+    console.log('Connected to collection : ' + db?.name)
     storage.value = db
   } else {
     console.warn('Something went wrong')
   }
 }
 
-const counter = ref(0);
-
-const increment = () => {
-  counter.value++;
+const fetchData = (): any => {
+  storage.value
+    .allDocs({
+      include_docs: true,
+    })
+    .then((result: any) => {
+      console.log('=> Données récupérées :', result.rows)
+      postsData.value = result.rows.map((row: any) => row.doc)
+    })
+    .catch((error: any) => {
+      console.error('Erreur lors de la récupération des données :', error)
+    })
 }
 
-const reset = () => {
-  counter.value = 0;
+const createDoc = (): any => {
+  counter++
+  storage.value
+    .post({
+      title: 'Document ' + counter,
+      content: 'Contenu du document : ' + words[Math.floor(Math.random() * words.length)],
+    })
+    .then(function (response: any) {
+      fetchData()
+      console.log(response)
+    })
+    .catch(function (err: any) {
+      console.log(err)
+    })
 }
 
+const deleteDoc = (post: Post): any => {
+  storage.value
+    .remove(post)
+    .then(function (response: any) {
+      fetchData()
+      console.log(response)
+    })
+    .catch(function (err: any) {
+      console.log(err)
+    })
+}
+
+const updateDoc = (post: Post): any => {
+  post.content = 'Contenu du document : ' + words[Math.floor(Math.random() * words.length)]
+  storage.value
+    .put(post)
+    .then(function (response: any) {
+      fetchData()
+      console.log(response)
+    })
+    .catch(function (err: any) {
+      console.log(err)
+    })
+}
+
+let counter = 0
+const words = [
+  'bonjour',
+  'spaghetti',
+  'casque',
+  'pull',
+  'vert',
+  'verre',
+  'vers',
+  'vair',
+  'léa',
+  'inoé',
+]
 </script>
 
 <template>
-  <h1>Hello World</h1>
-  <p style="font-size: 50px;">{{  counter  }}</p>
-  <button @click="increment">increment</button>
-  <button @click="reset">reset</button>
+  <h1>Fetch Data</h1>
+  <button @click="createDoc">Ajouter un document</button>
+  <article v-for="post in postsData" v-bind:key="(post as any).id">
+    <h2>{{ post.title }}</h2>
+    <p>{{ post.content }}</p>
+    <button @click="updateDoc(post)">Update</button>
+    <button @click="deleteDoc(post)">Effacer</button>
+  </article>
 </template>
