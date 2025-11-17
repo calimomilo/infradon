ff3cf9c7dc328f11aa00c95995666df3d5cbab82
Camilo <paula@pena-parra.ch>
2025-11-17 22:43:34 +0100

    fixed up styles so it stops moving all over the place

--- infradon2/src/App.vue
+++ infradon2/src/App.vue
@@ -20,11 +20,11 @@ header {
 }
 
 @media (min-width: 1024px) {
-  header {
+  /* header {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
-  }
+  } */
 
   .logo {
     margin: 0 2rem 0 0;
